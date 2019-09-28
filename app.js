0330c88b17c862c76f77324c773113c8039cc80a
AdrianGizycki <adrian.gizycki@gmail.com>
2019-09-28 10:23:43 +0200

    poprawki

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const btnToggle = document.querySelector(".btn3");
 const flipCard = document.querySelector('.flip-card');
 const base = document.querySelector('.data');
 const addBtn = document.querySelector('.add');
-console.log(addBtn);
+
 
 
 fetch('https://my-json-server.typicode.com/AdrianGizycki/Test2/data/')
@@ -17,9 +17,12 @@ fetch('https://my-json-server.typicode.com/AdrianGizycki/Test2/data/')
   .then((data) => {
     const min = 0
     var max = data.length;
+    let counter = 0;
+    divContents1.innerText = (data)[counter].english;
+    divContents2.innerText = (data)[counter].polish;
     document.addEventListener('keyup', function (e) {
       if (e.keyCode === 32) {
-      const counter = Math.floor(Math.random() * (max - min)) + min;
+      counter = Math.floor(Math.random() * (max - min)) + min;
       divContents1.innerText = (data)[counter].english;
       divContents2.innerText = (data)[counter].polish;
       }
@@ -28,51 +31,25 @@ fetch('https://my-json-server.typicode.com/AdrianGizycki/Test2/data/')
       console.log(data[0]);
       console.log((data)[1].english);
       console.log((data)[1].polish);
-    data.forEach(function(data) {
-      // console.log(data);
-      // console.log(data.english);
-      var dataId = (data.id);
-   //   console.log(data["id"]);
+    
       
     });
-
-  });
-
-  let inputE = document.querySelector('.inputEng').value;
-  let inputP = document.querySelector('.inputPl').value;
-  const formData = new FormData();
-  formData.append("english", "bird"),
-  formData.append("polish", "ptak"), 
- 
     
-function postData(event){
-  event.preventDefault();
-
-    fetch('https://my-json-server.typicode.com/AdrianGizycki/Test2/data/', {
-        method: 'POST',
-        body: formData
-    })
-    .then((res) => res.json())
-    .then(res => {
-      console.log(res);
-    })  
-};
-
-addBtn.addEventListener('click', function postData(){
+addBtn.addEventListener('submit', function postData(){
 });
 
 flipCard.addEventListener('click', () => {
-     console.log("test");
      flipCard.classList.toggle("flipped")
 });
 
-
-
-fetch('https://my-json-server.typicode.com/AdrianGizycki/Test2/data/', {
+function addData(e) {
+  const inputE = document.querySelector('.inputEng').value;
+  const inputP = document.querySelector('.inputPl').value;
+  fetch('https://my-json-server.typicode.com/AdrianGizycki/Test2/data/', {
     method: 'POST',
     body: JSON.stringify({
-      english: 'foo',
-      polish: 'bar',
+      english: inputE,
+      polish: inputP,
     }),
     headers: {
       "Content-type": "application/json; charset=UTF-8"
@@ -80,3 +57,10 @@ fetch('https://my-json-server.typicode.com/AdrianGizycki/Test2/data/', {
   })
   .then(response => response.json())
   .then(json => console.log(json))
+
+  e.preventDefault();
+}
+
+const form = document.querySelector('#form');
+form.addEventListener('submit', addData);
+
